54a124cf09b0334ecdd4eec5489a4007d7cf9817
alfaryolanda79 <alfaroyolanda@hotmail.com>
2023-10-04 18:32:38 +0200

    p4 absolute

--- p4-Calculadora/index.js
+++ p4-Calculadora/index.js
@@ -29,3 +29,25 @@ function division(){
     mostrarResultado(numero1 / numero2)
   
 }
+
+function raiz(){
+    let numero = +document.getElementById("campo2").value;
+    mostrarResultado(Math.sqrt(numero));
+  
+}
+
+function potencia(){
+    let numero1 = +document.getElementById("campo1").value;
+    let numero2 = +document.getElementById("campo2").value;
+    mostrarResultado(Math.pow(numero1, numero2));
+  
+}
+
+function absoluto(){
+    let numero = +document.getElementById("campo2").value;
+    mostrarResultado(Math.abs(numero));
+  
+}
+
+
+
