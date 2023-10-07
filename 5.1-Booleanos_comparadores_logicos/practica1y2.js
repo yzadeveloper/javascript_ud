f227fe6be71675c6f429d741becbf3b40aaaf1d1
alfaryolanda79 <alfaroyolanda@hotmail.com>
2023-10-07 17:19:16 +0200

    v55

---
+++ 5.1-Booleanos_comparadores_logicos/practica1y2.js
@@ -0,0 +1,17 @@
+//Implementar la función validar, para que valide si la edad pasada como parámetro 
+//esta entre 10 y 15 años. Guardar el resultado en la variable bool:
+let bool;
+
+function validar(edad) {
+    bool = edad >=10 && edad <= 15;
+    
+}
+
+//mplementar la función validar, que valide si la edad enviada por parámetro es 
+//igual a 10 o igual a 20. Guardar el resultado en la variable bool:
+let bool1;
+
+function validar(edad) {
+    bool1 = edad == 10 || edad == 20;
+    
+}
--- practicasChulas.js
+++ practicasChulas.js
@@ -25,3 +25,11 @@ function obtenerFecha(){
     return fechaActual.toLocaleDateString('es-ES');
     
 }
+
+//mplementar la funcion esMayorDeEdad, donde debe asignar a la variable bool 
+//el valor resultante de comprobar si la edad recibida por parámetro es igual o mayor a 18.
+let bool;
+
+function esMayorDeEdad(edad){
+    bool = edad > 18;
+}
