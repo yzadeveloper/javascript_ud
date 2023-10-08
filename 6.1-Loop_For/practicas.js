ccc6d098801a6b6900addab348c3908c027e915f
alfaryolanda79 <alfaroyolanda@hotmail.com>
2023-10-08 19:23:17 +0200

    v68 hasta do while

--- 6.1-Loop_For/practicas.js
+++ 6.1-Loop_For/practicas.js
@@ -41,3 +41,26 @@
 //     </body>
 // </html>
 
+// 3. implemente un Loop For cuya variable x asociada este inicializada en 1 y la condicion que se debe cumplir para que
+//el ciclo se siga ejecutando es que dicha variable sea menor a 5. Esto quiere decir, que el ciclo va a cumplir con 4 vueltas 
+//(iteraciones).
+//En cada vuelta del ciclo, se debe ir acumulando en la variable suma, el valor de la variable x. 
+//Ten en cuenta que estamos diciendo "acumular", lo que significa que debes sumar el valor que ya contiene la variabla suma, 
+//más el valor de x y ese resultado asignarlo a suma.
+
+let suma = 0;
+
+function testLoop() {
+    for( let x=1; x<5; x++){
+        suma = suma + x;
+    }
+   
+}
+
+//Implemente en la función testLoop, un Loop For que imprima en la consola la tabla de multiplicar (hasta el 10) del numero pasado por parámetro. 
+function testLoop(numero) {
+    for(let x=1; x<11; x++) {
+        console.log(numero*x);
+    }
+    
+}
