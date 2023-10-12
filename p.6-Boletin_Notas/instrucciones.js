083ad7e220d5ebe491d4913551532958b9f947bd
alfaryolanda79 <alfaroyolanda@hotmail.com>
2023-10-12 19:33:14 +0200

    inicio p6 Boletin notas

---
+++ p.6-Boletin_Notas/instrucciones.js
@@ -0,0 +1,23 @@
+//crear un array
+//mostrar 4 botones:
+    //mostrar lista con notas
+    //calcular promedio
+    //obtener nota más alta
+    //establecer si hubo suspensos
+
+//Mostrar notas:
+//funcion mostrarLista()
+//ul y li
+//loop for of
+
+//Promedio:
+//funcion calcularPromedio()
+//loop for
+
+//Nota más alta:
+//funcion notaMasAlta()
+//loop while (iniciar en  0, si no es 0 guardar en la variable)
+
+//Suspensos:
+//funcion haySuspensos()
+//loop do while
